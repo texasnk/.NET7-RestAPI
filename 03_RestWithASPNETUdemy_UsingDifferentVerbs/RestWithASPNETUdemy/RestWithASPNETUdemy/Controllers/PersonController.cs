64fa3f7fb68be8a39f8261a93dfe59668fd348a6
texasnk <davirodriguesnk@gmail.com>
2023-02-17 21:25:30 -0300

    Add UsingDifferentVerbs folder

---
+++ 03_RestWithASPNETUdemy_UsingDifferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+       
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+         
+           return BadRequest("Invalid input");
+        }
+        
+        
+
+    }
+}
