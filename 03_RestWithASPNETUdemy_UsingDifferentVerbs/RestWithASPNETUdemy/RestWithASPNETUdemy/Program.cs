80e3f6814da9d34b07ec6c915704de1b3fcc1d6d
texasnk <davirodriguesnk@gmail.com>
2023-02-19 00:11:55 -0300

    Add SQL connection

--- 03_RestWithASPNETUdemy_UsingDifferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
+++ 03_RestWithASPNETUdemy_UsingDifferentVerbs/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -1,17 +1,28 @@
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace RestWithASPNETUdemy
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
+            
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql
+            (connection, new MySqlServerVersion(new Version(8, 0, 25))));
+
             //Dependency injection
             builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
