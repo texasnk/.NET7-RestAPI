3950465feea28f65df3a29341a36023c264f9c39
texasnk <davirodriguesnk@gmail.com>
2023-02-22 03:14:27 -0300

    Update HATEOS

---
+++ 09_RestWithASPNETUdemy_HATEOS/RestWithASPNETUdemy/RestWithASPNETUdemy/Hypermedia/HypermediaLink.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace RestWithASPNETUdemy.Hypermedia
+{
+    public class HypermediaLink
+    {
+        public string Rel { get; set; }
+        private string href;
+        public string Href {
+            get {
+                object _lock = new object();
+                lock (_lock)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("2%F","/").ToString();
+                }
+            }
+            set { href = value; } }
+        public string Type { get; set; }
+        public string TAction { get; set; }
+
+    }
+}
