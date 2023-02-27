8590bb2a00e8c105dd44027cb033dbb28407f68e
texasnk <davirodriguesnk@gmail.com>
2023-02-27 05:41:32 -0300

    feat(auth): Add token interface for authentication

--- 12_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
+++ 12_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using RestWithASPNETUdemy.Data.VO;
 using RestWithASPNETUdemy.Model;
 using RestWithASPNETUdemy.Model.Context;
+using System.Data;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -19,6 +20,26 @@ namespace RestWithASPNETUdemy.Repository
         {
             var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
             return _context.Users.FirstOrDefault(u=>(u.UserName==user.UserName)&&(u.Password==pass));
+        }
+        public User RefreshUserInfo(User user)
+        {
+            if (!_context.Users.Any(u => u.id.Equals(user.id))) return null;
+            var result = _context.Users.SingleOrDefault(p => p.id.Equals(user.id));
+            
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return user;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return result;
         }
 
         private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
---
+++ 12_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/ITokenInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface ITokenInterface
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpireToken(string token);
+
+    }
+}
