3e64f0f0d46e3f74b50a47bf994c4ed563e883f0
texasnk <davirodriguesnk@gmail.com>
2023-03-01 11:26:30 -0300

    feat(auth): Implement AuthController

--- 12_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/AuthController.cs
+++ 12_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/AuthController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
 
 namespace RestWithASPNETUdemy.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [Route("api/[controller]/v{version:apiVersion")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private ILoginBusiness _loginBusiness;
+
+        public AuthController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult Signin([FromBody] UserVO user)
+        {
+            if (user == null) return BadRequest("Invalid client request");
+            var token = _loginBusiness.ValidateCredentials(user);
+            if (token == null) return Unauthorized();
+            return Ok(user);
+        }
     }
 }
