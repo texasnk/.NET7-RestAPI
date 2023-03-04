8feec70d69109772e42085eef15d7f9907ef6de3
texasnk <davirodriguesnk@gmail.com>
2023-03-04 14:25:44 -0300

    feat: Add new column to person table and create migration

---
+++ 13_RestWithASPNETUdemy_PATCH/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO : ISupportsHypermedia
+    {
+        public long id { get; set; }
+        public string NameBook { get; set; }
+
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+
+        public List<HypermediaLink> Links { get; set; } = new List<HypermediaLink>();
+    }
+}
