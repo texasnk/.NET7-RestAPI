37ccc1406e25b4bc9ab10f78fd2f037b1849d596
texasnk <davirodriguesnk@gmail.com>
2023-03-08 17:22:12 -0300

    feat: Add FileBusiness interface

---
+++ 15_RESTful_WorkingWithFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -0,0 +1,193 @@
+using RestWithASPNETUdemy.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Repository;
+using Serilog;
+using EvolveDb;
+using RestWithASPNETUdemy.Repository.Generic;
+using Microsoft.Net.Http.Headers;
+using RestWithASPNETUdemy.Hypermedia.Filters;
+using RestWithASPNETUdemy.Hypermedia.Enricher;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Rewrite;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementations;
+using RestWithASPNETUdemy.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace RestWithASPNETUdemy
+{
+    public class Program
+    {
+   
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            builder.Services.AddMvc(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+
+            }).AddXmlSerializerFormatters();
+
+            //
+
+            var tokenConfigurations = new TokenConfiguration();
+            new ConfigureFromConfigurationOptions<TokenConfiguration>(
+                builder.Configuration.GetSection("TokenConfigurations")
+                ).Configure(tokenConfigurations);
+
+            builder.Services.AddSingleton(tokenConfigurations);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = tokenConfigurations.Issuer,
+                    ValidAudience = tokenConfigurations.Audience,
+                    IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfigurations.Secret))
+                };
+            });
+
+            builder.Services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser().Build());
+            });
+
+            //
+
+            builder.Services.AddApiVersioning();
+
+            var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql
+            (connection, new MySqlServerVersion(new Version(8, 0, 25))));
+
+            if (builder.Environment.IsDevelopment())
+            {
+                MigrationDataBase(connection);
+            }
+
+            var filterOptions = new HypermediaFilterOptions();
+
+            filterOptions.ContentResponseEnricherList.Add(new PersonEnricher());
+            filterOptions.ContentResponseEnricherList.Add(new BookEnricher());
+
+
+            builder.Services.AddSingleton(filterOptions);
+
+          
+
+            //Dependency injection
+            builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            builder.Services.AddScoped<ILoginBusiness, LoginBusinessImplementation>();
+
+            builder.Services.AddTransient<ITokenService, TokenService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+
+
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new OpenApiInfo
+                {
+                    Title = "RestAPI with ASP .NET Core",
+                    Version = "v1.0",
+                    Description = "API RESTful developed for studies",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Texasnk",
+                        Url = new Uri("https://github.com/texasnk")
+                    }
+                });
+               
+            });
+
+            builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+                
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "REST API with ASP .NET Core");
+            });
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("DefaultApi", "{controller=values}/v{version=apiVersion}/{id?}");
+            });
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        private static void MigrationDataBase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" }, IsEraseDisabled = true,
+                };
+                evolve.OutOfOrder = true;
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed" + ex);
+                throw;
+            }
+        }
+    }
+}
