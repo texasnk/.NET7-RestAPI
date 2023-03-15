c686e034fbbbc02e5aa84d27adff1a8beced1efd
texasnk <davirodriguesnk@gmail.com>
2023-03-15 02:26:41 -0300

    feat: Add Azure database support

---
+++ 17_RESTful_Azure/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/FileController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Authorize("Bearer")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+
+    public class FileController : Controller
+    {
+        private readonly IFileBusiness _fileBusines;
+
+        public FileController(IFileBusiness fileBusines)
+        {
+            _fileBusines = fileBusines;
+        }
+
+        [HttpGet("downloadFile/{fileName}")]
+        [ProducesResponseType((200), Type = typeof(byte[]))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/octet-stream")]
+        public async Task<IActionResult> GetFileAsync(string fileName)
+        {
+            byte[] buffer = _fileBusines.GetFile(fileName);
+            if (buffer != null)
+            {
+                HttpContext.Response.ContentType = $"application/{Path.GetExtension(fileName).Replace(".", "")}";
+                HttpContext.Response.Headers.Add("content-length", buffer.Length.ToString());
+                await HttpContext.Response.Body.WriteAsync(buffer,0,buffer.Length);
+            }
+            return new ContentResult();
+        }
+        
+        [HttpPost("uploadFile")]
+        [ProducesResponseType((200), Type=typeof(FileDetailVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/json")]
+        public async Task<IActionResult> UploadOneFile([FromForm] IFormFile file)
+        {
+            FileDetailVO detail = await _fileBusines.SaveFileToDisk(file);
+            return new OkObjectResult(detail);
+        }
+
+        [HttpPost("uploadMultipleFiles")]
+        [ProducesResponseType((200), Type = typeof(List<FileDetailVO>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/json")]
+        public async Task<IActionResult> UploadMultipleFiles([FromForm] List<IFormFile> files)
+        {
+            List<FileDetailVO> details = await _fileBusines.SaveFilesToDisk(files);
+            return new OkObjectResult(details);
+        }
+
+    }
+}
